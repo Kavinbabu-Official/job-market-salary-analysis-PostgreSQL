7da303c4cd500dc66d31595875c2d31a37879dc9
kavin <tombeast1992@gmail.com>
2026-03-02 22:19:32 +0530

    Add files via upload

--- SQL/03_duplicate_removal.sql
+++ SQL/03_duplicate_removal.sql
@@ -1,1 +1,18 @@
-
+DELETE FROM insight i
+WHERE i.job_id IN (
+SELECT i.job_id FROM(
+SELECT  i.job_id,
+ROW_NUMBER() OVER(PARTITION BY
+		i.job_title,
+		i.company_name,
+		i.location,
+		i.experience_required_years,
+		i.skills,
+		i.salary_min_inr,
+		i.salary_max_inr,
+		i.employment_type,
+		i.remote_option 
+ORDER BY i.job_id ) AS rn
+FROM insight i ) t
+WHERE rn > 1
+);
