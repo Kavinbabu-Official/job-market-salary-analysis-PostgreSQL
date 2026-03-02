e6bd22ebc6d0a2dca540dd534c4ee5136c390b13
kavin <tombeast1992@gmail.com>
2026-03-02 21:03:24 +0530

    Add files via upload

--- SQL/01_create_table.sql
+++ SQL/01_create_table.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE insight (
+job_id INT PRIMARY KEY,
+job_title VARCHAR(50),
+company_name VARCHAR(50),
+location VARCHAR(50),
+experience_required_years INT,
+skills VARCHAR(100),
+salary_min_inr DECIMAL(10,2),
+salary_max_inr DECIMAL(10,2),
+employment_type VARCHAR(50),
+remote_option VARCHAR(50)
+);
