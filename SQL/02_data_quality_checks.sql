53d95eeb2f9ae78497080ad90d9a7ec696785508
kavin <tombeast1992@gmail.com>
2026-03-02 21:08:45 +0530

    Add files via upload

--- SQL/02_data_quality_checks.sql
+++ SQL/02_data_quality_checks.sql
@@ -1,1 +1,23 @@
+-- Total Records:
 
+SELECT 
+	COUNT(*)
+FROM
+	insight;
+
+-- Missing Values Check:
+
+SELECT
+	COUNT(*) AS incomplete_records
+FROM insight i 
+WHERE 
+	i.job_id IS NULL OR
+	i.job_title IS NULL OR
+	i.company_name IS NULL OR
+	i.location IS NULL OR
+	i.experience_required_years IS NULL OR
+	i.skills IS NULL OR
+	i.salary_min_inr IS NULL OR
+	i.salary_max_inr IS NULL OR
+	i.employment_type IS NULL OR
+	i.remote_option IS NULL;
